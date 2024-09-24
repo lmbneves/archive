486e8cb04516b64987ea839136c005c78ed39598
lmbneves <lmbneves@gmail.com>
2024-09-24 15:21:05 -0400

    init app structure

---
+++ src/screens/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, SafeAreaView } from 'react-native';
+
+function Home(): React.JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <Button
+        title="Add new archive"
+      />
+    </SafeAreaView>
+  );
+}
+
+export default Home;
