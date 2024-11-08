3fd5c453e038218efc9e6c63bea7d9b60d1e4c68
lmbneves <lmbneves@gmail.com>
2024-11-08 16:30:02 -0500

    Create archive via bottom sheet modal

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,14 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { Button, SafeAreaView, ScrollView, View, Pressable } from 'react-native';
+import React, { useCallback, useEffect, useMemo, useState, useRef } from 'react';
+import { Button, SafeAreaView, ScrollView, View, Pressable, StyleSheet } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import {
+  BottomSheetModal,
+  BottomSheetView,
+  BottomSheetTextInput,
+  BottomSheetModalProvider,
+} from '@gorhom/bottom-sheet';
 import { RootStackParamList  } from '../navigation/types';
 import { ArchiveEntryComponent } from '../components/ArchiveEntry';
-import { AddModal } from '../components/AddModal';
 import { Archive } from '../models'
 import { getDBConnection, createTable, getArchives, saveArchives, deleteArchive } from '../services/db-service';
 import uuid from 'react-native-uuid';
@@ -13,7 +18,9 @@ type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 const HomeScreen: React.FC<Props> = ({ navigation }) => {
   const [archives, setArchives] = useState<Archive[]>([]);
   const [newArchive, setNewArchive] = useState('');
-  const [modalVisible, setModalVisible] = useState(false);
+
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const snapPoints = useMemo(() => ["15%"], []);
 
   const loadDataCallback = useCallback(async () => {
     try {
@@ -33,21 +40,33 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
       console.error(error);
     }
   }, []);
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+  const handlePresentModalDismiss = useCallback(() => {
+    bottomSheetModalRef.current?.dismiss();
+  }, []);
+  const handleSheetChanges = useCallback((index: number) => {
+    // console.log('handleSheetChanges', index);
+  }, []);
 
-  const handleModalNewArchive = (name: string) => {
-    addArchiveEntry(name);
-    setModalVisible(false);
+  const handleCreateArchiveSubmit = () => {
+    addArchiveEntry();
+    // chain these actions?
+    handlePresentModalDismiss();
   };
-  const addArchiveEntry = async (name: string) => {
-    if (!name.trim()) return;
+
+  const addArchiveEntry = async () => {
+    handlePresentModalPress();
+    if (!newArchive.trim()) return;
     try {
       const newArchives = [...archives, {
-        id: uuid.v4() as string, name: name
+        id: uuid.v4() as string, name: newArchive
       }];
       setArchives(newArchives);
       const db = await getDBConnection();
       await saveArchives(db, newArchives)
-      // setNewArchive('');
+      setNewArchive('');
     } catch (error) {
       console.error(error);
     }
@@ -67,35 +86,62 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
   }, [loadDataCallback]);
   
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View>
-          {archives.map((archive) => (
-            <Pressable
-              key={archive.id}
-              onPress={() => 
-                navigation.navigate('Archive', { archive: archive })
-              }
-            >
-              <ArchiveEntryComponent key={archive.id} archive={archive} deleteArchiveEntry={deleteArchiveEntry} />
-            </Pressable>
-          ))}
-        </View>
-        <View>
-          <Button
-            onPress={() => setModalVisible(true)}
-            color="#841584"
-            accessibilityLabel="open add archive modal"
-            title="Create archive"
-          />
-          <AddModal
-            isVisible={modalVisible}
-            setInputValue={handleModalNewArchive}
-          />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <BottomSheetModalProvider>
+      <SafeAreaView>
+        <ScrollView>
+          <View>
+            {archives.map((archive) => (
+              <Pressable
+                key={archive.id}
+                onPress={() => 
+                  navigation.navigate('Archive', { archive: archive })
+                }
+              >
+                <ArchiveEntryComponent key={archive.id} archive={archive} deleteArchiveEntry={deleteArchiveEntry} />
+              </Pressable>
+            ))}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+      <Button
+        onPress={handlePresentModalPress}
+        title="Create archive"
+        color="black"
+      />
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        onChange={handleSheetChanges}
+        snapPoints={snapPoints}
+      >
+        <BottomSheetView style={styles.contentContainer}>
+          <View>
+            <BottomSheetTextInput
+              style={styles.textInput}
+              value={newArchive}
+              onChangeText={setNewArchive}
+              onSubmitEditing={handleCreateArchiveSubmit}
+              placeholder="Name archive..."
+              autoFocus
+            />
+          </View>
+        </BottomSheetView>
+      </BottomSheetModal>
+    </BottomSheetModalProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+    marginLeft: 15,
+    marginRight: 15
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginBottom: 10,
+  },
+});
 
 export default HomeScreen;
